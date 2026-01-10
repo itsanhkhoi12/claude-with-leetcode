ab22120bbc94bbc2d7cfb09cb8df61a94108c15c
Edwardz <hanchivi39@gmail.com>
2026-01-10 20:37:36 +1100

    Daily Leetcode Solution

---
+++ Array_And_Hashing/1470_Shuffle.py
@@ -0,0 +1,13 @@
+# LeetCode 1470: Shuffle
+from typing import List
+
+class Solution:
+    class Solution:
+        def shuffle(self, nums: List[int], n: int) -> List[int]:
+            ans = []
+    
+            for i in range(n):
+                ans.append(nums[i])
+                ans.append(nums[i + n])
+            return ans
+    
